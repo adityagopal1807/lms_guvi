8f31284273c4b8bdb8ad9a5917303f21b7ed92a2
adityagopal1807 <160997178+adityagopal1807@users.noreply.github.com>
2024-12-09 16:24:30 +0530

    Create UserDAO.java

---
+++ java/UserDAO.java
@@ -0,0 +1,44 @@
+package com.lms.dao;
+
+import com.lms.model.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    private Connection connection;
+
+    public UserDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void addUser(User user) throws SQLException {
+        String query = "INSERT INTO users (name, email, role, password) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getRole());
+            stmt.setString(4, user.getPassword());
+            stmt.executeUpdate();
+        }
+    }
+
+    public List<User> getAllUsers() throws SQLException {
+        List<User> users = new ArrayList<>();
+        String query = "SELECT * FROM users";
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setRole(rs.getString("role"));
+                user.setPassword(rs.getString("password"));
+                users.add(user);
+            }
+        }
+        return users;
+    }
+}
